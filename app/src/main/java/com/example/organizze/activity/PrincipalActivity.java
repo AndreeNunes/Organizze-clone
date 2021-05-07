4a7e932706a036a727ec610dd6b064552e02a221
André Nicola Nunes <57879149+AndreeNunes@users.noreply.github.com>
2021-05-06 22:27:28 -0300

    add sign out

--- app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
@@ -3,6 +3,7 @@ package com.example.organizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.organizze.config.ConfigurancaoFirebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -10,10 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.organizze.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -23,6 +26,7 @@ public class PrincipalActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao;
     private TextView textoSaldo;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,18 @@ public class PrincipalActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao = ConfigurancaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarReceita(View view){
